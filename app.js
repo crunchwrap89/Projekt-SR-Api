fcdcae4a87d033edc252e44bcb0169bd3b6c6536
crunchwrap89 <marcus89n@gmail.com>
2020-04-23 12:21:28 +0200

    mergeconflict, removed unneccessary code

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-
- fetch("http://api.sr.se/api/v2/programs?format=json")
-
-    .then(response => response.json())
-    .then(json => console.log(json))
-
-knapp1.addEventListener('click', () => {
-// metod att starta här     
-})
-knapp2.addEventListener('click', () => {
-// metod att starta här 
-})
-knapp3.addEventListener('click', () => {
-// metod att starta här
-})
-knapp4.addEventListener('click', () => {
-// metod att starta här
-})
-
-
-async function playRadioChannel() {
-
-}
-async function pauseRadioChannel() {
-
-}
-
 import ActionBar from './components/EventKnappar.js'
 import ErrorToast from './components/EventError.js'
 
