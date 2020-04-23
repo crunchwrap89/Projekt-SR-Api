f1ccb1513161dc65b6425dcfe8433059172c6a78
crunchwrap89 <marcus89n@gmail.com>
2020-04-23 12:14:52 +0200

    removed code from app.js

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-import eventknappar from './components/EventKnappar.js'
-
- fetch("http://api.sr.se/api/v2/programs?format=json")
-
-    .then(response => response.json())
-    .then(json => console.log(json))
-
-knapp1.addEventListener('click', () => {
-// metod att starta här     
-})
-knapp2.addEventListener('click', () => {
-// metod att starta här 
-})
-knapp3.addEventListener('click', () => {
-// metod att starta här
-})
-knapp4.addEventListener('click', () => {
-// metod att starta här
-})
-
-
-async function playRadioChannel() {
-
-}
-async function pauseRadioChannel() {
-
-}
-
-
 new Vue({
     el: '#app',
     data: {
