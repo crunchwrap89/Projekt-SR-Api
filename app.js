9f7c371736637c7c025f828816bc78e5d679475d
crunchwrap89 <marcus89n@gmail.com>
2020-04-23 11:19:47 +0200

    changed appstructure, added folders and files

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
     fetch("http://api.sr.se/api/v2/programs?format=json")
-      .then(response => response.json())
-      .then(json => console.log(json))
- 
+    .then(response => response.json())
+    .then(json => console.log(json))
+
 knapp1.addEventListener('click', () => {
-  // metod att starta här     
+// metod att starta här     
 })
 knapp2.addEventListener('click', () => {
-  // metod att starta här 
+// metod att starta här 
 })
 knapp3.addEventListener('click', () => {
- // metod att starta här
+// metod att starta här
 })
 knapp4.addEventListener('click', () => {
- // metod att starta här
+// metod att starta här
 })
-  
+
 
 async function playRadioChannel() {
 
