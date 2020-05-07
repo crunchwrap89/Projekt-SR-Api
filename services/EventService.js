e1457c8ab53582d0c0d157244fd8ef1a8051aeae
unknown <blixtdackhammar@gmail.com>
2020-05-07 10:12:21 +0200

    Add method for fetching songlists

--- services/EventService.js
+++ services/EventService.js
@@ -11,6 +11,24 @@ function getRadioChannel(input) {
         .then(json => json.channel.liveaudio.url)
 }
 
+function getSongList(input) {
+    return fetch(input + dateString())
+        .then(resp =>  {
+            if (!resp.ok) {
+                throw new Error(resp.statusText)
+            }
+            return resp.json()
+        })
+        .then(json => json.songlist)
+}
+
+function dateString() {
+    let y = getFullYear();
+    let m = getMonth();
+    let d = getDate();
+    return y + "-" + m + "-" + d;
+}
+
 export default {
     playP1() {
         return getRadioChannel('http://api.sr.se/api/v2/channels/132?format=json')
@@ -26,6 +44,22 @@ export default {
 
     playP4() {
         return getRadioChannel('http://api.sr.se/api/v2/channels/212?format=json')
+    },
+
+    songListP1() {
+
+    },
+
+    songListP2() {
+
+    },
+
+    songListP3() {
+
+    },
+
+    songListP4() {
+
     }
 
 }
