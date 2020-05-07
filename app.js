8f46188720f7508c0de5e2dade5697bd1a113d81
crunchwrap89 <marcus89n@gmail.com>
2020-05-07 09:48:17 +0200

    added router-pages

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ import EventError from './components/EventError.js'
 Vue.component('action-bar', EventKnappar)
 Vue.component('toast', EventError)
 
+const Home = { template: '<p>my home page</p>' }
+const About = { template: '<p>my about page</p>' }
+const router = new VueRouter({routes: [ { path: '/', component: Home },                 
+                             { path: '/about', component: About } ]    })
+
+
+
 new Vue({
     el: '#app',
+    router,
     data: {
       message: 'Välj en radiokanal!'
     }
