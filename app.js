7b1f0d7d9878fc7de9dc4463640206b9c4f71e7a
crunchwrap89 <marcus89n@gmail.com>
2020-05-07 12:11:51 +0200

    merge conflict

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ import EventError from './components/EventError.js'
 Vue.component('action-bar', EventKnappar)
 Vue.component('toast', EventError)
 
-const Home = { template: `<div><h1>Home</h1><nav>
-                              <router-link tag="li" class="col" class-active="active" to="/radio">Radio</router-link>
-                              <router-link tag="li" class="col" class-active="active" to="/about">About</router-link>
+const Home = { template: `<div><nav>
+                              <router-link tag="ul" class="col" class-active="active" to="/radio">Radio</router-link>
+                              <router-link tag="ul" class="col" class-active="active" to="/about">About</router-link>
                           </nav>
+                            <h1>Home</h1>
                             <p> 
                               Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
                               Nulla mazim viris sed cu, sententiae definitionem eam in. Ei nam oratio legendos, assum essent latine sed ea, has ei tantas pertinacia. Tempor persius salutatus te his, no primis ceteros his. Cu pri dicat dicit conclusionemque, dicat deleniti scribentur eam no. Sonet choro no vis, eum ad wisi aperiri albucius, qui quis sale scaevola eu.
@@ -22,17 +23,19 @@ const Home = { template: `<div><h1>Home</h1><nav>
                               In vide sadipscing sit, epicuri facilisi te mea. Te quidam repudiandae qui, ius ne quas mucius inimicus. Pri possit sensibus eloquentiam at, eum te commodo diceret iudicabit. Has ei vocent vivendum, laudem numquam graecis id usu. Dolorem hendrerit ad ius. Est primis habemus at, ei duo cibo cetero virtute.
                             </p>
                           </div>` }
-const Radio = { template: `<div><h1>Radio</h1><nav>
-                              <router-link tag="li" class="col" class-active="active" to="/">Home</router-link>
-                              <router-link tag="li" class="col" class-active="active" to="/about">About</router-link>
+const Radio = { template: `<div><nav>
+                              <router-link tag="ul" class="col" class-active="active" to="/">Home</router-link>
+                              <router-link tag="ul" class="col" class-active="active" to="/about">About</router-link>
                           </nav>
+                            <h1>Radio</h1>
                             <action-bar></action-bar>
                             <audio id ="ljudspelare" src="http://sverigesradio.se/topsy/direkt/srapi/164.mp3" autoplay="" controls="" style="height: 40px; width: 66%;"></audio>
                           </div>` }
-const About = { template: `<div><h1>About</h1><nav>
-                              <router-link tag="li" class="col" class-active="active" to="/">Home</router-link>
-                              <router-link tag="li" class="col" class-active="active" to="/radio">Radio</router-link>
+const About = { template: `<div><nav>
+                              <router-link tag="ul" class="col" class-active="active" to="/">Home</router-link>
+                              <router-link tag="ul" class="col" class-active="active" to="/radio">Radio</router-link>
                             </nav>
+                            <h1>About</h1>
                             <p>Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
                               Nulla mazim viris sed cu, sententiae definitionem eam in. Ei nam oratio legendos, assum essent latine sed ea, has ei tantas pertinacia. Tempor persius salutatus te his, no primis ceteros his. Cu pri dicat dicit conclusionemque, dicat deleniti scribentur eam no. Sonet choro no vis, eum ad wisi aperiri albucius, qui quis sale scaevola eu.
                               Ea quaeque erroribus quo. Eu vim facilisi recusabo constituto, eu duo ignota rationibus. Ne simul melius prodesset has, qui omnesque volutpat et. Et vel modo tollit pericula, probo alienum eos an. Ne ius impetus elaboraret, et paulo audiam duo, eum natum utroque ei.
@@ -45,7 +48,10 @@ const router = new VueRouter({
         { path: '/', component: Home },
         { path: '/radio', component: Radio },
         { path: '/about', component: About } 
-    ]})
+    ],
+    linkActiveClass: "active",
+    linkExactActiveClass: "exact-active"
+  })
 
 let app = new Vue({
     el: '#app',
