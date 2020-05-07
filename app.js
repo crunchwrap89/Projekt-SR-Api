55514542ebabf63fcf1432b2f084a900b9f678a5
crunchwrap89 <marcus89n@gmail.com>
2020-05-07 10:51:38 +0200

    tried some things with the router-link design

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ Vue.component('action-bar', EventKnappar)
 Vue.component('toast', EventError)
 
 const Home = { template: `<div><p>My home page</p><nav>
-                              <router-link to="/radio">Radio</router-link>
-                              <router-link to="/about">About</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/radio">Radio</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/about">About</router-link>
                           </nav></div>` }
 const Radio = { template: `<div><p>Radio</p><nav>
-                              <router-link to="/">Home</router-link>
-                              <router-link to="/about">About</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/">Home</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/about">About</router-link>
                           </nav>
                             <action-bar></action-bar>
                             <audio id ="ljudspelare" src="http://sverigesradio.se/topsy/direkt/srapi/164.mp3" autoplay="" controls="" style="height: 40px; width: 66%;"></audio>
                           </div>` }
 const About = { template: `<div><p>About</p><nav>
-                              <router-link to="/">Home</router-link>
-                              <router-link to="/radio">Radio</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/">Home</router-link>
+                              <router-link tag="li" class="col" class-active="active" to="/radio">Radio</router-link>
                             </nav></div>` }
 const router = new VueRouter({
       routes: [ 
