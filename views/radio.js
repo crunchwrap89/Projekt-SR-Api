77133946067f8ea0c6fa47c297e84a83da1cf287
crunchwrap89 <marcus89n@gmail.com>
2020-05-10 14:02:44 +0200

    added images and tested some designthemes

--- views/radio.js
+++ views/radio.js
@@ -1,7 +1,13 @@
-export default { template: `<div class="outerdiv">
-                              <h1>Radio</h1>
-                            <div class="innerdiv">
-                              <action-bar></action-bar>
-                              <audio id="ljudspelare" autoplay="" controls="" style="height: 40px; width: 66%;"></audio>
-                            </div>
-                          </div>` }
+export default { template: 
+`<div class="outerdiv">
+   <h1>Radio</h1>
+     <div class="innerdiv">
+        <div class="vansterdiv">
+          <h2> Just nu spelas:  </h2>
+          <audio id="ljudspelare" autoplay="" controls="" style="height: 40px; width: 66%;"></audio>
+        </div>
+        <div class="hogerdiv">
+          <action-bar></action-bar>
+        </div>
+     </div>
+ </div>` }
