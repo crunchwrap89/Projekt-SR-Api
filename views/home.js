2a89b8d54bfb141bf903f8166aaefbcfeb4de24a
crunchwrap89 <marcus89n@gmail.com>
2020-05-11 10:04:58 +0200

    added graphics

--- views/home.js
+++ views/home.js
@@ -7,7 +7,7 @@ export default { template: `<div class="outerdiv">
                               Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
                               Nulla mazim viris sed cu, sententiae definitionem eam in.  
                             </p>
-                            <h1> Riots emerge in Germany- Frankfurt all out of sauerkraut</h1>
+                            <h1> Riots emerge in Germany - Frankfurt all out of sauerkraut</h1>
                             <img src="../images/riot.jpg" alt="riot" style="width:100%;max-width:800px">
 
                             </div>
