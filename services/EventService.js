238846a31e955fde9c62fa10f135ba97ffab97da
unknown <blixtdackhammar@gmail.com>
2020-05-14 16:06:00 +0200

    Shortened some methods. Removed some methods that could be made into one.

--- services/EventService.js
+++ services/EventService.js
@@ -40,21 +40,36 @@ function getNewsPrograms(input) {
 }
 
 export default {
-    playP1() {
-        return getRadioChannel('http://api.sr.se/api/v2/channels/132?format=json')
+    play(channel) {
+        if(channel = p1) {
+            return getRadioChannel('http://api.sr.se/api/v2/channels/132?format=json')  
+        }
+        if(channel = p2) {
+            return getRadioChannel('http://api.sr.se/api/v2/channels/163?format=json')
+        }
+        if(channel = p3) {
+            return getRadioChannel('http://api.sr.se/api/v2/channels/164?format=json')
+        }
+        if(channel = p4) {
+            return getRadioChannel('http://api.sr.se/api/v2/channels/212?format=json') 
+        }
     },
 
-    playP2() {
-        return getRadioChannel('http://api.sr.se/api/v2/channels/163?format=json')
-    },
+    // playP1() {
+    //     return getRadioChannel('http://api.sr.se/api/v2/channels/132?format=json')
+    // },
 
-    playP3() {
-        return getRadioChannel('http://api.sr.se/api/v2/channels/164?format=json')
-    },
+    // playP2() {
+    //     return getRadioChannel('http://api.sr.se/api/v2/channels/163?format=json')
+    // },
 
-    playP4() {
-        return getRadioChannel('http://api.sr.se/api/v2/channels/212?format=json')
-    },
+    // playP3() {
+    //     return getRadioChannel('http://api.sr.se/api/v2/channels/164?format=json')
+    // },
+
+    // playP4() {
+    //     return getRadioChannel('http://api.sr.se/api/v2/channels/212?format=json')
+    // },
 
     showNewsPrograms() {
         return getNewsPrograms('http://api.sr.se/api/v2/news/episodes?format=json')
--- views/radio.js
+++ views/radio.js
@@ -5,19 +5,19 @@ export default {
           play(radiokanal) {
                let p = document.getElementById("ljudspelare")
                let i = document.getElementById("nuspelas")
-               let e = ""
-               if (radiokanal === 1) {
-                    e = EventService.playP1()
-               }
-               if (radiokanal === 2) {
-                    e = EventService.playP2()
-               }
-               if (radiokanal === 3) {
-                     e = EventService.playP3()
-               }
-               if (radiokanal === 4) {
-                     e = EventService.playP4()
-               }
+               let e = EventService.play(radiokanal)
+               // if (radiokanal === 1) {
+               //      e = EventService.playP1()
+               // }
+               // if (radiokanal === 2) {
+               //      e = EventService.playP2()
+               // }
+               // if (radiokanal === 3) {
+               //       e = EventService.playP3()
+               // }
+               // if (radiokanal === 4) {
+               //       e = EventService.playP4()
+               // }
                e.then(function (result) {
                    p.setAttribute("src", result.liveaudio.url)
                    i.setAttribute("src", result.image)
@@ -28,10 +28,10 @@ export default {
 `<div class="outerdiv">
      <div class="innerdiv">
      <div class="flex-container">
-        <div class="p1">Radio P1 <img class="playbtn" src="../images/playbtn.png" @click="play(1)"></div>
-        <div class="p2">Radio P2 <img class="playbtn" src="../images/playbtn.png" @click="play(2)"></div>
-        <div class="p3">Radio P3 <img class="playbtn" src="../images/playbtn.png" @click="play(3)"></div>  
-        <div class="p4">Radio P4 <img class="playbtn" src="../images/playbtn.png" @click="play(4)"></div>
+        <div class="p1">Radio P1 <img class="playbtn" src="../images/playbtn.png" @click="play(p1)"></div>
+        <div class="p2">Radio P2 <img class="playbtn" src="../images/playbtn.png" @click="play(p2)"></div>
+        <div class="p3">Radio P3 <img class="playbtn" src="../images/playbtn.png" @click="play(p3)"></div>  
+        <div class="p4">Radio P4 <img class="playbtn" src="../images/playbtn.png" @click="play(p4)"></div>
         <div>LocalNewZ</div>
         <div>LocalNewZ6</div>  
         <div>LocalNewZ7</div>
