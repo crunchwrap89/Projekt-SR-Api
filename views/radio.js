0ca925dc39af544f7a148129048f31c053a08b9b
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-14 15:53:37 +0200

    made all the methods in radio in to one

--- views/radio.js
+++ views/radio.js
@@ -1,52 +1,37 @@
 import EventService from '../services/EventService.js'
 
-export default { 
+export default {
      methods: {
-          playP1() {
-              let p = document.getElementById("ljudspelare")
-              let i = document.getElementById("nuspelas")
-              let e = EventService.playP1()
-              e.then(function (result) {
-                  p.setAttribute("src", result.liveaudio.url)
-                  i.setAttribute("src", result.image)
-              })
-          },
-          playP2() {
-              let p = document.getElementById("ljudspelare")
-              let i = document.getElementById("nuspelas")
-              let e = EventService.playP2()
-              e.then(function (result) {
-                  p.setAttribute("src", result.liveaudio.url)
-                  i.setAttribute("src", result.image)
-              })
-          },
-          playP3() {
-              let p = document.getElementById("ljudspelare")
-              let i = document.getElementById("nuspelas")
-              let e = EventService.playP3()
-              e.then(function (result) {
-                  p.setAttribute("src", result.liveaudio.url)
-                  i.setAttribute("src", result.image)
-              })
-          },
-          playP4() {
-              let p = document.getElementById("ljudspelare")
-              let i = document.getElementById("nuspelas")
-              let e = EventService.playP4()
-              e.then(function (result) {
-                  p.setAttribute("src", result.liveaudio.url)
-                  i.setAttribute("src", result.image)
-              })
-          },
+          play(radiokanal) {
+               let p = document.getElementById("ljudspelare")
+               let i = document.getElementById("nuspelas")
+               let e = ""
+               if (radiokanal === 1) {
+                    e = EventService.playP1()
+               }
+               if (radiokanal === 2) {
+                    e = EventService.playP2()
+               }
+               if (radiokanal === 3) {
+                     e = EventService.playP3()
+               }
+               if (radiokanal === 4) {
+                     e = EventService.playP4()
+               }
+               e.then(function (result) {
+                   p.setAttribute("src", result.liveaudio.url)
+                   i.setAttribute("src", result.image)
+               })
+           },
       },
      template: 
 `<div class="outerdiv">
      <div class="innerdiv">
      <div class="flex-container">
-        <div class="p1">Radio P1 <img class="playbtn" src="../images/playbtn.png" @click="playP1"></div>
-        <div class="p2">Radio P2 <img class="playbtn" src="../images/playbtn.png" @click="playP2"></div>
-        <div class="p3">Radio P3 <img class="playbtn" src="../images/playbtn.png" @click="playP3"></div>  
-        <div class="p4">Radio P4 <img class="playbtn" src="../images/playbtn.png" @click="playP4"></div>
+        <div class="p1">Radio P1 <img class="playbtn" src="../images/playbtn.png" @click="play(1)"></div>
+        <div class="p2">Radio P2 <img class="playbtn" src="../images/playbtn.png" @click="play(2)"></div>
+        <div class="p3">Radio P3 <img class="playbtn" src="../images/playbtn.png" @click="play(3)"></div>  
+        <div class="p4">Radio P4 <img class="playbtn" src="../images/playbtn.png" @click="play(4)"></div>
         <div>LocalNewZ</div>
         <div>LocalNewZ6</div>  
         <div>LocalNewZ7</div>
