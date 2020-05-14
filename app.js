37d5047a2721b0d4f147134931d59f780bc835a7
crunchwrap89 <marcus89n@gmail.com>
2020-05-14 14:06:29 +0200

    moved eventknappar

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-import EventKnappar from './components/EventKnappar.js'
+
 import EventError from './components/EventError.js'
 import News from './components/news.js'
 import router from './router/index.js'
 
 Vue.component('news', News)
-Vue.component('action-bar', EventKnappar)
 Vue.component('toast', EventError)
 
 let app = new Vue({
--- views/radio.js
+++ views/radio.js
@@ -1,9 +1,56 @@
-export default { template: 
+import EventService from '../services/EventService.js'
+
+export default { 
+     methods: {
+          playP1() {
+              let p = document.getElementById("ljudspelare")
+              let i = document.getElementById("nuspelas")
+              let e = EventService.playP1()
+              e.then(function (result) {
+                  p.setAttribute("src", result.liveaudio.url)
+                  i.setAttribute("src", result.image)
+              })
+          },
+          playP2() {
+              let p = document.getElementById("ljudspelare")
+              let i = document.getElementById("nuspelas")
+              let e = EventService.playP2()
+              e.then(function (result) {
+                  p.setAttribute("src", result.liveaudio.url)
+                  i.setAttribute("src", result.image)
+              })
+          },
+          playP3() {
+              let p = document.getElementById("ljudspelare")
+              let i = document.getElementById("nuspelas")
+              let e = EventService.playP3()
+              e.then(function (result) {
+                  p.setAttribute("src", result.liveaudio.url)
+                  i.setAttribute("src", result.image)
+              })
+          },
+          playP4() {
+              let p = document.getElementById("ljudspelare")
+              let i = document.getElementById("nuspelas")
+              let e = EventService.playP4()
+              e.then(function (result) {
+                  p.setAttribute("src", result.liveaudio.url)
+                  i.setAttribute("src", result.image)
+              })
+          },
+          songListP1() {
+              let p = document.getElementById("ljudspelare")
+              let e = EventService.songListP1()
+              e.then(function (result) {
+                  p.setAttribute("src", result)
+              })
+          },
+      },
+     template: 
 `<div class="outerdiv">
      <div class="innerdiv">
      <div class="flex-container">
-        <div class="p1">Radio P1
-        <input type="button" @click="playP1" value="Radio P1"></div>
+        <div class="p1">Radio P1 <input type="button" @click="playP1" value="Radio P1"></div>
         <div class="p2">Radio P2</div>
         <div class="p3">Radio P3</div>  
         <div class="p4">Radio P4</div>
