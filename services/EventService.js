7099d7ddd3ce9bad3a9127d4de72c0670237d228
unknown <blixtdackhammar@gmail.com>
2020-05-15 16:10:37 +0200

    ost

--- services/EventService.js
+++ services/EventService.js
@@ -68,7 +68,7 @@ export default {
     },
 
     getDocumentaries() {
-        return getData('http://api.sr.se/api/v2/podfiles?programid=1646&format=json')
+        return getData('http://api.sr.se/api/v2/podfiles?programid=2519&format=json')
     }
 
 }
