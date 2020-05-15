dc97a51121ed960613fe65e293f16b07abc3a9d2
unknown <blixtdackhammar@gmail.com>
2020-05-15 11:53:34 +0200

    bice

--- router/index.js
+++ router/index.js
@@ -2,15 +2,15 @@ import Home from '../views/home.js'
 import Radio from '../views/radio.js'
 import Archive from '../views/archive.js'
 import About from '../views/about.js'
-import Dokumentar from '..views/dokumentar.js'
+// import Dokumentar from '..views/dokumentar.js'
 
 export default new VueRouter({
     routes: [ 
       { path: '/', component: Home },
       { path: '/radio', component: Radio },
       { path: '/archive', component: Archive },
-      { path: '/about', component: About },
-      { path: '/dokumentar', component: Dokumentar } 
+      { path: '/about', component: About }
+      // { path: '/dokumentar', component: Dokumentar } 
   ],
   linkActiveClass: "active",
   linkExactActiveClass: "exact-active",
