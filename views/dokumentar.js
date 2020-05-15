75690d7e054c895150b51dd3bfd071386f47e5c2
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-15 15:27:06 +0200

    pew

--- views/dokumentar.js
+++ views/dokumentar.js
@@ -1,27 +1,16 @@
 import EventService from "../services/EventService.js"
 
 export default {
-    created() {
-        this.fetchData()
-   },
-   watch: {
-        $route(to, from) {
-             this.fetchData()
-        },
-   },
+  
     methods: {
-        fetchData() {
-            this.getDocumentaries()
-        },
-
         getDocumentaries() {
             let outerdiv = document.getElementById('kuk')
             let d = EventService.getDocumentaries()
 
             d.then(function (result) {
-                for(let i = 0; result.length; i++) {
+                for(let i = 0; i < result.podfiles.length; i++) {
                     let p = document.createElement('p')
-                    p.innerText = result[i].title
+                    p.innerText = result.podfiles[i].title
                     outerdiv.appendChild(p)
                 }
             })
