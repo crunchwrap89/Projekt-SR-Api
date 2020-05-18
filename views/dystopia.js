71ea4651c6f6a41ced1016573fad0059ec24d728
crunchwrap89 <marcus89n@gmail.com>
2020-05-18 12:30:00 +0200

    added classnames

--- views/dystopia.js
+++ views/dystopia.js
@@ -12,14 +12,15 @@ export default {
             let l = document.getElementById("ljudspelare")
             d.then(function (result) {
                 for(let i = 0; i < result.episodes.length; i++) {
-                    let p = document.createElement('p')
-                    p.innerText = result.episodes[i].title
-                    p.addEventListener( 'click', function(){
+                    let div = document.createElement('div')
+                    div.innerText = result.episodes[i].title
+                    div.addEventListener( 'click', function(){
                         l.setAttribute("src", result.episodes[i].downloadpodfile.url)
                         i.setAttribute("src", result.episodes[i].imageurl)
+                        div.className('dAvsnitt')
 
                       } );
-                    outerdiv.appendChild(p)
+                    outerdiv.appendChild(div)
                 }
             })
         }
@@ -32,3 +33,15 @@ export default {
      </div>
  </div>` 
 }
+
+// `<div class="outerdiv">
+//      <div class="innerdiv">
+//      <h1> Radio channels </h1>
+//      <div class="flex-container">
+//         <div class="p1"><img class="playbtn" src="../images/playbtn.png" @click="play(1)"></div>
+//         <div class="p2"><img class="playbtn" src="../images/playbtn.png" @click="play(2)"></div>
+//         <div class="p3"><img class="playbtn" src="../images/playbtn.png" @click="play(3)"></div>  
+//         <div class="p4"><img class="playbtn" src="../images/playbtn.png" @click="play(4)"></div>  
+//      </div>
+//      </div>
+//  </div>`
