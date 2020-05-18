c6913639239a0af54ef7a04ef7da50496dcde589
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-18 12:30:14 +0200

    added ekonomiEkot

---
+++ views/ekonomiEkot.js
@@ -0,0 +1,29 @@
+import EventService from "../services/EventService.js"
+
+export default {
+    mounted() {
+        this.getEkonomiEko()
+    },
+    methods: {
+        getEkonomiEko() {
+            let outerdiv = document.getElementById('superId')
+            let d = EventService.getEkonomiEko()
+            let l = document.getElementById("ljudspelare")
+            d.then(function (result) {
+                for(let i = 0; i < result.episodes.length; i++) {
+                    let p = document.createElement('div')
+                    p.innerText = result.episodes[i].title
+                    p.addEventListener( 'click', function(){
+                        l.setAttribute("src", result.episodes[i].downloadpodfile.url)
+                      } );
+                    outerdiv.appendChild(p)
+                }
+            })
+        }        
+    },  
+    template: 
+`<div class="outerdiv" id="superId">
+     <div class="innerdiv">
+     </div>
+ </div>` 
+}
