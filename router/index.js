1b8b56563a5b699e479adae4c07421eb0aeceef2
crunchwrap89 <marcus89n@gmail.com>
2020-05-18 11:52:25 +0200

    prepared code for nightmode

--- router/index.js
+++ router/index.js
@@ -29,3 +29,22 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}// function to toggle between light and dark theme
+function toggleTheme() {
+ if (localStorage.getItem('theme') === 'theme-dark'){
+     setTheme('theme-light');
+ } else {
+     setTheme('theme-dark');
+ }
+}// Immediately invoked function to set the theme on initial load
+(function () {
+ if (localStorage.getItem('theme') === 'theme-dark') {
+     setTheme('theme-dark');
+ } else {
+     setTheme('theme-light');
+ }
+})();
