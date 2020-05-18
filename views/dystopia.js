226b3dbfa11ed22dba657a2236099c87c63bdc54
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-18 11:30:29 +0200

    added links to pod list

--- views/dystopia.js
+++ views/dystopia.js
@@ -8,11 +8,16 @@ export default {
         getDystopia() {
             let outerdiv = document.getElementById('superId')
             let d = EventService.getDystopia()
-
+            let l = document.getElementById("ljudspelare")
             d.then(function (result) {
                 for(let i = 0; i < result.episodes.length; i++) {
                     let p = document.createElement('p')
                     p.innerText = result.episodes[i].title
+                    p.addEventListener( 'click', function(){
+                        l.setAttribute("src", result.episodes[i].downloadpodfile.url)
+                        
+
+                      } );
                     outerdiv.appendChild(p)
                 }
             })
