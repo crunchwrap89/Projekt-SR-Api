737e4ebb45747a7bc038e4e88c4e662e72cf8dab
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-19 11:38:18 +0200

    fixed try catch

--- views/kulturnytt.js
+++ views/kulturnytt.js
@@ -11,14 +11,21 @@ export default {
             let img = document.getElementById("nuspelas")
             let l = document.getElementById("ljudspelare")   
             d.then(function (result) {
-                console.log(result.episodes[1].broadcast.broadcastfiles[0].url)
                 for(let i = 0; i < result.episodes.length; i++) {
                     let div = document.createElement('div')
-                    div.innerText = result.episodes[i].downloadpodfile.title
-                    div.addEventListener( 'click', function(){
-                        l.setAttribute("src", result.episodes[i].downloadpodfile.url)
-                        img.setAttribute("src", result.episodes[i].imageurl)
-                      } );
+                    try {
+                        div.innerText = result.episodes[i].downloadpodfile.title
+                        div.addEventListener( 'click', function(){
+                            l.setAttribute("src", result.episodes[i].downloadpodfile.url)
+                            img.setAttribute("src", result.episodes[i].imageurl)
+                          } );
+                    } catch {
+                        div.innerText = result.episodes[i].title
+                        div.addEventListener( 'click', function(){
+                            l.setAttribute("src", result.episodes[i].broadcast.broadcastfiles[0].url)
+                            img.setAttribute("src", result.episodes[i].imageurl)
+                          } );
+                    }
                     div.className="dAvsnitt"
                     div.style.backgroundImage="url(" + result.episodes[i].imageurl + ")"
                     roffel.appendChild(div)
