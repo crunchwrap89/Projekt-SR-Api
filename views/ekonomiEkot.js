cc6ad757cf8915e30d9798454fa92267030ce1c1
crunchwrap89 <marcus89n@gmail.com>
2020-05-19 09:47:30 +0200

    fixed layoutcode for ekonomiekot and radiosporten

--- views/ekonomiEkot.js
+++ views/ekonomiEkot.js
@@ -6,30 +6,35 @@ export default {
     },
     methods: {
         getEkonomiEkot() {
-            let outerdiv = document.getElementById('nyttId')
+            let roffel = document.getElementById('nyttId')
             let d = EventService.getEkonomiEkot()
             let l = document.getElementById("ljudspelare")
             let img = document.getElementById("nuspelas")
             d.then(function (result) {
                 for(let i = 0; i < result.episodes.length; i++) {
-                    let p = document.createElement('div')
+                    let div = document.createElement('div')
                     let titeln = result.episodes[i].downloadpodfile.title
-                    p.innerText = titeln
-                    if (p.innerText.charAt(10) === "t") {
-                        p.innerText = titeln.slice(0, 10) + titeln.slice(11, titeln.length)
+                    div.innerText = titeln
+                    if (div.innerText.charAt(10) === "t") {
+                        div.innerText = titeln.slice(0, 10) + titeln.slice(11, titeln.length)
                     }                     
-                    p.addEventListener( 'click', function(){
+                    div.addEventListener( 'click', function(){
                         l.setAttribute("src", result.episodes[i].downloadpodfile.url)
                         img.setAttribute("src", result.episodes[i].imageurl)
                       } );
-                    outerdiv.appendChild(p)
+                    div.className="dAvsnitt"
+                    div.style.backgroundImage="url(" + result.episodes[i].imageurl + ")"
+                    roffel.appendChild(div)
                 }
             })
         }        
     },  
     template: 
-`<div class="outerdiv" id="nyttId">
-     <div class="innerdiv">
-     </div>
- </div>` 
+    `<div class="outerdiv">
+    <div class="innerdiv">
+          <h1> Kulturnytt avsnitt </h1>
+      <div class="flex-container" id="nyttId">
+      </div>
+    </div>
+</div>`
 }
