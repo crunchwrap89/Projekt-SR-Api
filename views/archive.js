4b86187ab23952b41ea32742d99ea1081f0ef90b
crunchwrap89 <marcus89n@gmail.com>
2020-05-19 13:57:42 +0200

    added classnames for routerlinks on archive

--- views/archive.js
+++ views/archive.js
@@ -7,28 +7,28 @@ export default {
             <h1>Gamla sändningar</h1>
             <div class="flex-container">
                   <div class="n4">
-                  <router-link to="/ekonomiEkot">Ekonomiekot</router-link>
+                  <router-link to="/ekonomiEkot" class="arkivknapp">Ekonomiekot</router-link>
                   </div>
                   <div class="n3">
-                  <router-link to="/radiosporten">Radiosporten</router-link>
+                  <router-link to="/radiosporten" class="arkivknapp">Radiosporten</router-link>
                   </div>
                   <div class="n5">
-                  <router-link to="/samiskaNyheter">Samiska Nyheter</router-link>
+                  <router-link to="/samiskaNyheter" class="arkivknapp">Samiska Nyheter</router-link>
                   </div>
                   <div class="n2">
-                  <router-link to="/radiofinska">Finska Nyheter</router-link>
+                  <router-link to="/radiofinska" class="arkivknapp">Finska Nyheter</router-link>
                   </div>
                   <div class="n1">
-                  <router-link to="/kulturnytt">Kulturnytt</router-link>
+                  <router-link to="/kulturnytt" class="arkivknapp">Kulturnytt</router-link>
                   </div>
               </div>
               <h1>Podcasts</h1>
               <div class="flex-container2">
                   <div>
-                  <router-link to="/dystopia">P3 Dystopia</router-link>
+                  <router-link to="/dystopia" class="arkivknapp">P3 Dystopia</router-link>
                   </div>
                   <div>
-                  <router-link to="/dokumentar">P3 Dokumentär</router-link>
+                  <router-link to="/dokumentar" class="arkivknapp">P3 Dokumentär</router-link>
                   </div>
               </div>
          </div>
