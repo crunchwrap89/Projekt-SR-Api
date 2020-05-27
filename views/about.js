2f7eea6bdc4bcdda6f6d7668165697f1f23b709a
crunchwrap89 <marcus89n@gmail.com>
2020-05-27 09:03:54 +0200

    made changes to style and aboutpage

--- views/about.js
+++ views/about.js
@@ -1,28 +1,30 @@
 export default { template: `<div class="outerdiv">
                              <div class="innerdiv2">
                               <div class="vansterdiv">
-                            <h1> Rubrik </h1>
-                            <p> 
-                              Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
-                              Nulla mazim viris sed cu, sententiae definitionem eam in. Ei nam oratio legendos, assum essent latine sed ea, has ei tantas pertinacia. Tempor persius salutatus te his, no primis ceteros his. Cu pri dicat dicit conclusionemque, dicat deleniti scribentur eam no. Sonet choro no vis, eum ad wisi aperiri albucius, qui quis sale scaevola eu.
-                              Ea quaeque erroribus quo. Eu vim facilisi recusabo constituto, eu duo ignota rationibus. Ne simul melius prodesset has, qui omnesque volutpat et. Et vel modo tollit pericula, probo alienum eos an. Ne ius impetus elaboraret, et paulo audiam duo, eum natum utroque ei.
-                              Vim cibo paulo putant ad, id vis solet interesset, pri an mucius euripidis. At nec error putant mentitum, aliquando forensibus argumentum te est. Vel ad ludus nostro signiferumque, eam in sumo elitr assueverit. Cu mei possit noluisse. Eripuit moderatius cu vim.
-                              In vide sadipscing sit, epicuri facilisi te mea. Te quidam repudiandae qui, ius ne quas mucius inimicus. Pri possit sensibus eloquentiam at, eum te commodo diceret iudicabit. Has ei vocent vivendum, laudem numquam graecis id usu. Dolorem hendrerit ad ius. Est primis habemus at, ei duo cibo cetero virtute.
-                            <br><br>
-                              Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
-                              Nulla mazim viris sed cu, sententiae definitionem eam in. Ei nam oratio legendos, assum essent latine sed ea, has ei tantas pertinacia. Tempor persius salutatus te his, no primis ceteros his. Cu pri dicat dicit conclusionemque, dicat deleniti scribentur eam no. Sonet choro no vis, eum ad wisi aperiri albucius, qui quis sale scaevola eu.
-                              Ea quaeque erroribus quo. Eu vim facilisi recusabo constituto, eu duo ignota rationibus. Ne simul melius prodesset has, qui omnesque volutpat et. Et vel modo tollit pericula, probo alienum eos an. Ne ius impetus elaboraret, et paulo audiam duo, eum natum utroque ei.
-                              Vim cibo paulo putant ad, id vis solet interesset, pri an mucius euripidis. At nec error putant mentitum, aliquando forensibus argumentum te est. Vel ad ludus nostro signiferumque, eam in sumo elitr assueverit. Cu mei possit noluisse. Eripuit moderatius cu vim.
-                              In vide sadipscing sit, epicuri facilisi te mea. Te quidam repudiandae qui, ius ne quas mucius inimicus. Pri possit sensibus eloquentiam at, eum te commodo diceret iudicabit. Has ei vocent vivendum, laudem numquam graecis id usu. Dolorem hendrerit ad ius. Est primis habemus at, ei duo cibo cetero virtute.
+                                <h1> Om Radio Roffel </h1>
+                                <p> 
+                                  Radio Roffel är en single page Vue.js-applikation som använder sig utav Sveriges Radios öppna API (version 2). <br>
+                                  Den använder sig utav 3st olika endpoints: Program, Ljud och Nyheter - för att kunna streama liveradio, hämta bilder, podcasts och arkiverade nyhetsprogram. 
+                              <br>För mer information om API't: 
+                              <a href="https://sverigesradio.se/api/documentation/v2/index.html">Klicka här</a>
+                              <br><br>
+                                  Applikationen är responsiv och den har en Themeswitch-funktion med cssvariabler. <br>
+                                  Navigationspanelen högst upp innehåller router-links som pekar till olika vyer och navigationspanelen har även stilsatts med en transition vid scroll nedåt. <br>
+                                  Applikationen har testats och verifierats i Chrome, Firefox, Safari and Edge i flera olika upplösningsvariationer.
+                              <br><br>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
                             </p>
                             </div>
                             <div class="hogerdiv">
-                            <h1>Rubrik 2</h1>
-                            <p>
-                            Lorem ipsum dolor sit amet, ex erant oratio iuvaret sea, sit ne dicit utinam molestie, ne sit nusquam percipit. Vis ea adhuc nonumy facilisi, tractatos splendide suscipiantur et mel. Errem sapientem at mel, mel cu vocent epicurei persecuti. Pri tempor copiosae platonem at, fastidii mediocritatem in pro, vix iudico integre prompta cu.
-                            Nulla mazim viris sed cu, sententiae definitionem eam in. Ei nam oratio legendos, assum essent latine sed ea, has ei tantas pertinacia. Tempor persius salutatus te his, no primis ceteros his. Cu pri dicat dicit conclusionemque, dicat deleniti scribentur eam no. Sonet choro no vis, eum ad wisi aperiri albucius, qui quis sale scaevola eu.
-                            Ea quaeque erroribus quo. Eu vim facilisi recusabo constituto, eu duo ignota rationibus. Ne simul melius prodesset has, qui omnesque volutpat et. Et vel modo tollit pericula, probo alienum eos an. Ne ius impetus elaboraret, et paulo audiam duo, eum natum utroque ei.
-                            </p>
+                            <h1>Utvecklare</h1>
+                            <div class="flex-containerAbout">
+                              <div class="profilbild1">Erik Blixt-Dackhammar<img class="erikPb" src="../images/erik.jpg"></div>
+                              <div class="profilbild2">Marcus Nilsson<img class="marcusPb" src="../images/mac.jpg"></div>
+                              <div class="profilbild3">Anders Ivansson<img class="andersPb" src="../images/anders2.jpeg"></div> 
+                            </div>
+
+                            
                             </div>
                             </div>
                           </div>` }
